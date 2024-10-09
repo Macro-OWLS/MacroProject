c5f2867eeff1f99d57dcff2904956458e11e106f
Agfi <thoriq.aghfi60@gmail.com>
2024-10-09 15:28:55 +0700

    feat: add color palette

---
+++ MacroProject/Utilities/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  MacroProject
+//
+//  Created by Agfi on 09/10/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static var blue: Color = Color("Blue")
+    static var cream: Color = Color("Cream")
+    static var yellow: Color = Color("Yellow")
+    static var orange: Color = Color("Orange")
+    static var green: Color = Color("Green")
+    static var purple: Color = Color("Purple")
+    static var pink: Color = Color("Pink")
+}
