ec30f3dcd6593045236814d3c3eb4bdd27137f81
Agfi <thoriq.aghfi60@gmail.com>
2024-10-11 14:14:02 +0700

    fix: fix topic repository logic and add sync manager

--- MacroProject/Data/Repositories/TopicRepository.swift
+++ MacroProject/Data/Repositories/TopicRepository.swift
@@ -12,12 +12,16 @@ import SwiftData
 internal protocol TopicRepositoryType {
     func fetch() -> AnyPublisher<[TopicModel]?, NetworkError>
     func create(param: TopicModel) -> AnyPublisher<Bool, NetworkError>
+    func update(param: TopicModel) -> AnyPublisher<Bool, NetworkError>
     func delete(id: String) -> AnyPublisher<Bool, NetworkError>
 }
 
 internal final class TopicRepository: TopicRepositoryType {
     private let container = SwiftDataContextManager.shared.container
     private let dataSynchronizer = DataSynchronizer()
+    private let supabase = SupabaseService.shared.getClient()
+    
+    private var hasSynchronized: Bool = false
     
     init() { }
     
@@ -25,11 +29,9 @@ internal final class TopicRepository: TopicRepositoryType {
         return Future<[TopicModel]?, NetworkError> { promise in
             Task { @MainActor in
                 do {
-                    let fetchDescriptor = FetchDescriptor<TopicEntity>()
-                    let topic = try self.container?.mainContext.fetch(fetchDescriptor)
-                    let models = topic?.compactMap { $0.toDomain() }
-                   
-                    promise(.success(models))
+                    try await self.ensureSynchronized()
+                    let topics = try self.fetchFromLocal()
+                    promise(.success(topics))
                 } catch {
                     promise(.failure(.noData))
                 }
@@ -42,9 +44,21 @@ internal final class TopicRepository: TopicRepositoryType {
         return Future<Bool, NetworkError> { promise in
             Task { @MainActor in
                 do {
-                    let entity = TopicEntity(id: param.id, name: param.name, desc: param.desc, isAddedToLibraryDeck: param.isAddedToLibraryDeck)
-                    self.container?.mainContext.insert(entity)
-                    try self.container?.mainContext.save()
+                    try self.createLocal(param: param)
+                    promise(.success(true))
+                } catch {
+                    promise(.failure(.noData))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func update(param: TopicModel) -> AnyPublisher<Bool, NetworkError> {
+        return Future<Bool, NetworkError> { promise in
+            Task { @MainActor in
+                do {
+                    try self.updateLocal(param: param)
                     promise(.success(true))
                 } catch {
                     promise(.failure(.noData))
@@ -58,30 +72,126 @@ internal final class TopicRepository: TopicRepositoryType {
         return Future<Bool, NetworkError> { promise in
             Task { @MainActor in
                 do {
-                    let fetchDescriptor = FetchDescriptor<PhraseCardEntity>(predicate: #Predicate { $0.id == id })
-                    let result = Result {
-                        do {
-                            if let entity = try self.container?.mainContext.fetch(fetchDescriptor).first {
-                                self.container?.mainContext.delete(entity)
-                                try self.container?.mainContext.save()
-                                return true
-                            } else {
-                                throw NetworkError.noData
-                            }
-                        } catch {
-                            throw error
-                        }
-                    }
-                    
-                    switch result {
-                    case .success(let response):
-                        promise(.success(response))
-                    case .failure(let error):
-                        promise(.failure(.genericError(error: error)))
-                    }
+                    try self.deleteLocal(id: id)
+                    promise(.success(true))
+                } catch {
+                    promise(.failure(.noData))
                 }
             }
         }
         .eraseToAnyPublisher()
+    }
+}
+
+
+// MARK: Local Repository
+extension TopicRepository {
+    @MainActor private func fetchFromLocal() throws -> [TopicModel]? {
+        let fetchDescriptor = FetchDescriptor<TopicEntity>()
+        let topics = try container?.mainContext.fetch(fetchDescriptor)
+        return topics?.compactMap { $0.toDomain() }
+    }
+    
+    @MainActor private func createLocal(param: TopicModel) throws {
+        let entity = TopicEntity(id: param.id, name: param.name, desc: param.desc, isAddedToLibraryDeck: param.isAddedToLibraryDeck)
+        self.container?.mainContext.insert(entity)
+        try self.container?.mainContext.save()
+    }
+    
+    @MainActor private func updateLocal(param: TopicModel) throws {
+        let id = param.id
+        let fetchDescriptor = FetchDescriptor<TopicEntity>(predicate: #Predicate { $0.id == id })
+        if let entity = try container?.mainContext.fetch(fetchDescriptor).first {
+            entity.name = param.name
+            entity.desc = param.desc
+            entity.isAddedToLibraryDeck = param.isAddedToLibraryDeck
+            try container?.mainContext.save()
+        } else {
+            throw NetworkError.noData
+        }
+    }
+    
+    @MainActor private func deleteLocal(id: String) throws {
+        let fetchDescriptor = FetchDescriptor<TopicEntity>(predicate: #Predicate { $0.id == id })
+        if let entity = try container?.mainContext.fetch(fetchDescriptor).first {
+            container?.mainContext.delete(entity)
+            try container?.mainContext.save()
+        } else {
+            throw NetworkError.noData
+        }
+    }
+}
+
+// MARK: Remote Repository
+extension TopicRepository {
+    private func fetchRemote() async throws -> [TopicModel] {
+        do {
+            let fetchedTopic: [TopicModel] = try await supabase
+                .database
+                .from("Topics")
+                .select()
+                .execute()
+                .value
+            
+            if !fetchedTopic.isEmpty {
+                return fetchedTopic
+            }
+        } catch {
+            return []
+        }
+        
+        return []
+    }
+    
+    private func createRemote(param: TopicModel) async throws {
+        do {
+            try await supabase
+                .database
+                .from("Topics")
+                .insert([
+                    "id": param.id,
+                    "name": param.name,
+                    "desc": param.desc,
+                    "isAddedToLibraryDeck": param.isAddedToLibraryDeck ? "true" : "false"
+                ]).execute()
+        } catch {
+            throw NetworkError.noData
+        }
+    }
+    
+    private func updateRemote(param: TopicModel) async throws {
+        do {
+            try await supabase
+                .database
+                .from("Topics")
+                .update([
+                    "name": param.name,
+                    "desc": param.desc,
+                    "isAddedToLibraryDeck": param.isAddedToLibraryDeck ? "true" : "false"
+                ])
+                .eq("id", value: param.id)
+                .execute()
+        } catch {
+            throw NetworkError.noData
+        }
+    }
+}
+
+// MARK: UTILITIES FUNCTION
+extension TopicRepository {
+    private func ensureSynchronized() async throws {
+        guard !self.hasSynchronized && SyncManager.isFirstAppOpen() else { return
+        }
+        
+        try await self.synchronizeRemoteToLocal()
+        SyncManager.markAsSynchronized()
+        self.hasSynchronized = true
+    }
+    
+    private func synchronizeRemoteToLocal() async throws {
+        let remoteTopics = try await self.fetchRemote()
+        for topic in remoteTopics {
+            try await self.createLocal(param: topic)
+        }
     }
 }
