dd531e54f593ecc402bb0704d864569ade2738fd
Agestia Deanara Tori <agestia.dtori@gmail.com>
2024-10-14 22:20:34 +0700

    feat: add vocabSearch
    function to bold/blank a vocab in a phrase

--- MacroProject/Helper/PhraseHelper.swift
+++ MacroProject/Helper/PhraseHelper.swift
@@ -7,8 +7,27 @@
 
 import Foundation
 
+enum VocabEdit {
+    case bold
+    case blank
+}
+
 final class PhraseHelper {
+    
     static func filterPhrases(by topicID: String, from phrases: [PhraseCardModel]) -> [PhraseCardModel] {
         return phrases.filter { $0.topicID == topicID }
     }
+    
+    func vocabSearch(phrase: String, vocab: String, vocabEdit: VocabEdit) -> String {
+         
+        switch vocabEdit {
+        case .bold:
+            return phrase.replacingOccurrences(of: vocab, with: "**\(vocab)**" )
+        case .blank:
+            return phrase.replacingOccurrences(of: vocab, with: "_____" )
+        }
+       
+    }
 }
+
+
