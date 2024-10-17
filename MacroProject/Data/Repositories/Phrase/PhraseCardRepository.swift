9305ca9dbc856de8ac3a6968c1df3cbf01c03d42
Agestia Deanara Tori <agestia.dtori@gmail.com>
2024-10-17 12:49:32 +0700

    chore: adjust date update func to be more dynamic to results (correct/incorrect)

--- MacroProject/Data/Repositories/Phrase/PhraseCardRepository.swift
+++ MacroProject/Data/Repositories/Phrase/PhraseCardRepository.swift
@@ -15,7 +15,7 @@ internal protocol PhraseCardRepositoryType {
     func fetchPhrase(topicID: String, levelNumber: String) -> AnyPublisher<[PhraseCardModel]?, NetworkError>
     func create(param: PhraseCardModel) -> AnyPublisher<Bool, NetworkError>
     func delete(id: String) -> AnyPublisher<Bool, NetworkError>
-    func update(id: String) -> AnyPublisher<Bool, NetworkError>
+    func update(id: String, result: PhraseResult) -> AnyPublisher<Bool, NetworkError>
 }
 
 internal final class PhraseCardRepository: PhraseCardRepositoryType {
@@ -109,11 +109,11 @@ internal final class PhraseCardRepository: PhraseCardRepositoryType {
         .eraseToAnyPublisher()
     }
     
-    func update(id: String) -> AnyPublisher<Bool, NetworkError> {
+    func update(id: String, result: PhraseResult) -> AnyPublisher<Bool, NetworkError> {
         return Future<Bool, NetworkError> { promise in
             Task { @MainActor in
                 do {
-                    try await self.localRepository.updatePhrase(id: id)
+                    try await self.localRepository.updatePhrase(id: id, result: result)
                     promise(.success(true))
                 } catch {
                     promise(.failure(.noData))
