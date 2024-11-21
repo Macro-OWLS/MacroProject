215a81e55e6038fc743d58112e27bfec99f94b56
Agestia Deanara Tori <agestia.dtori@gmail.com>
2024-11-22 00:15:02 +0700

    fix: fix add topic color

--- MacroProject/Presentation/Onboarding/ViewModel/OnboardingViewModel.swift
+++ MacroProject/Presentation/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -171,7 +171,6 @@ internal final class OnboardingViewModel: ObservableObject {
                 self.updateCooldown()
                 self.userInputTarget = ""
                 self.isDisabled = true
-                print("\nupdate target viewmodel\n ")
             }
         } catch {
             DispatchQueue.main.async {
